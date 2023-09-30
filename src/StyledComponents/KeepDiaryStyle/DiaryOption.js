87cce1c11ac201c3de4decff15ca90b5aeb91fe6
audwo <kmjstj3@naver.com>
2023-09-30 15:10:53 +0900

    KeepDiaryStyle

---
+++ src/StyledComponents/KeepDiaryStyle/DiaryOption.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const DiaryAdvanceSetupWrapper = styled.div`
+  width: 400px;
+  height: 200px;
+  margin-top: 20px;
+`;
+
+export const DiaryOptionImg = styled.div`
+  width: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+`;
+
+export const DiaryOptionPublic = styled.div`
+  width: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+`;
