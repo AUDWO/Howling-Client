1b3d72b82437b184d957afebd9bf39618c773407
audwo <kmjstj3@naver.com>
2023-09-30 15:04:02 +0900

    DiaryAdvancedSetup

---
+++ src/StyledComponents/KeepDiaryStyle/DiaryAdvancedSetupComponent.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Ooption = styled.div`
+  width: 350px;
+  height: 210px;
+  margin-bottom: 100px;
+  padding: 10px 5px 10px 20px;
+  border-radius: 10px;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const LikeCountAbleButtonTitle = styled.div``;
+
+export const HitsAbleButtonTitle = styled.div``;
+
+export const CommentsAbleButtonTtile = styled.div``;
+
+export const ToggleAbleButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
