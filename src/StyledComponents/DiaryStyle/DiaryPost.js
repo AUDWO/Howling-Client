d9e9374c928ff0822c07d106ff617032b75d3dad
audwo <kmjstj3@naver.com>
2023-10-18 19:30:39 +0900

    New style of diaryPage

--- src/StyledComponents/DiaryStyle/DiaryPost.js
+++ src/StyledComponents/DiaryStyle/DiaryPost.js
@@ -3,9 +3,8 @@ import styled from "styled-components";
 export const DiaryPostWrapper = styled.div`
   width: 300px;
   height: 370px;
-  margin-left: 25px;
-  margin-right: 25px;
-  margin-bottom: 100px;
+  margin-bottom: 10px;
+  position: relative;
   border-radius: 5px;
   box-shadow: rgba(0, 0, 0, 0.04) 0px 4px 16px 0px;
   transition: transform 0.3s;
@@ -19,7 +18,6 @@ export const DiaryPostsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   background-color: #f8f9fa;
-
   justify-content: flex-start;
   padding: 20px;
 `;
@@ -27,28 +25,52 @@ export const DiaryPostsWrapper = styled.div`
 export const DiaryPostImgWrapper = styled.img`
   background-color: skyblue;
   width: 100%;
-  height: 60%;
+  height: 50%;
+  position: absolute;
+  top: 0;
 `;
 
 //DiaryPostInfo
 
 export const DiaryPostInfoWrapper = styled.div`
   width: 100%;
-  height: 40%;
+  height: 50%;
+  position: absolute;
+  bottom: 0;
 `;
 
 export const DiaryPostTitleWrapper = styled.div`
+  height: 60%;
+  font-weight: 600;
   padding: 10px 20px 10px 20px;
-  font-size: 30px;
-  font-family: "Nanum Pen Script", cursive;
 `;
 
 export const DiaryPostUserWrapper = styled.div`
+  width: 100%;
+  height: 20%;
   border-top: 0.5px solid #959ca3;
-  padding: 10px 0px 0px 20px;
+  padding-left: 5%;
+  display: flex;
+  align-items: center;
 `;
 
+export const UserProfileWrapper = styled.div`
+  display: flex;
+  align-center: center;
+  width: 1.8em;
+  height: 1.8em;
+  border-radius: 50%;
+  background-color: black;
+  margin-right: 0.5em;
+`;
+export const UserNickname = styled.span`
+  font-size: 13px;
+`;
 export const DiaryPostDate = styled.div`
+  display: flex;
+  align-items: center;
+  height: 20%;
+  line-height: 0.6;
   color: #959ca3;
   font-weight: 100;
   font-size: 18px;
