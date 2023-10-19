4871f85a04fb24e2ea58d225a7304ef0a532529d
audwo <kmjstj3@naver.com>
2023-10-19 15:41:47 +0900

    게시물, 일기 타입 별 분리

--- src/Components/Profile/ProfileCp.js
+++ src/Components/Profile/ProfileCp.js
@@ -1,27 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-
 import ProfileInfoCp from "./ProfileInfoCp";
-import ProfilePostsCp from "./ProfilePostsCp";
+import ProfilePostsInfoCp from "./ProfilePostsInfoCp";
+import NoPostContentCp from "./NoPostContentCp";
+import { useRecoilValue } from "recoil";
+import toggleValueAtom from "../../store/ToggleValueAtom";
+import axios from "axios";
 
 const ProfileCp = () => {
+  const postContentOpen = useRecoilValue(toggleValueAtom("postType"));
+  const diaryContentOpen = useRecoilValue(toggleValueAtom("diaryType"));
+
+  const [contents, setContents] = useState([]);
+
+  useEffect(() => {
+    const handleType = () => {
+      if (postContentOpen) {
+        return "posts";
+      }
+      if (diaryContentOpen) {
+        return "Diaries";
+      }
+    };
+    const fetchContentsData = async () => {
+      try {
+        const response = await axios.get(`/page/${handleType()}`);
+        setContents([...response.data]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchContentsData();
+  }, [postContentOpen, diaryContentOpen]);
+
   return (
     <ProfilePageWrapper>
       <ProfileWrapper>
         <ProfileImg />
         <ProfileInfoCp />
       </ProfileWrapper>
-      <PostsInfoWrapper>
-        <PostCountWrapper>
-          <PostCountTitle>게시물</PostCountTitle>
-          <PostCountNumber>0</PostCountNumber>
-        </PostCountWrapper>
-        <DiaryCountWrapper>
-          <DiaryCountTitle>일기</DiaryCountTitle>
-          <DiaryCountNumber>0</DiaryCountNumber>
-        </DiaryCountWrapper>
-      </PostsInfoWrapper>
-      <ProfilePostsCp />
+      <ProfilePostsInfoCp />
+      {contents.length >= 1 ? (
+        <ContentsWrapper>
+          <ContentCardsWrapper>
+            {contents.map((content) => {
+              <ContentCard src={content.img} />;
+            })}
+          </ContentCardsWrapper>
+        </ContentsWrapper>
+      ) : (
+        <>
+          {postContentOpen && (
+            <NoPostContentCp
+              title={"게시물 올리기"}
+              content={"게시물을 올리면 회원님의 프로필에 표시됩니다."}
+              subtitle={"게시물 올리기"}
+            />
+          )}
+          {diaryContentOpen && (
+            <NoPostContentCp
+              title={"일기 쓰기"}
+              content={"일기를 쓰면 회원님의 프로필에 표시됩니다."}
+              subtitle={"일기 쓰기"}
+            />
+          )}
+        </>
+      )}
     </ProfilePageWrapper>
   );
 };
@@ -30,11 +75,11 @@ export default ProfileCp;
 
 export const ProfilePageWrapper = styled.div`
   flex: 1;
-  height: auto;
-  border: 5px solid red;
+  height: 100vh;
 `;
 
 export const ProfileWrapper = styled.div`
+  height: 35%;
   display: flex;
   padding: 40px 60px 40px 60px;
 `;
@@ -46,42 +91,26 @@ export const ProfileImg = styled.img`
   background-color: black;
 `;
 
-export const PostsInfoWrapper = styled.div`
-  border-top: 0.5px solid #dbdbdb;
+//------
+
+export const ContentsWrapper = styled.div`
+  border: 1px solid black;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
 `;
 
-export const PostCountWrapper = styled.div`
-  display: flex;
-  align-items: center;
+export const ContentCardsWrapper = styled.div`
+  width: auto;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(300px, 1fr));
+  grid-gap: 10px;
+  border: 1px solid red;
 `;
 
-export const PostCountTitle = styled.div`
-  padding-top: 10px;
-  border-top: 1.5px solid black;
-  font-size: 15px;
-`;
-
-export const PostCountNumber = styled.div`
-  font-size: 15px;
-  font-weight: 700;
-  margin-left: 10px;
-  padding-top: 10px;
-`;
-
-export const DiaryCountWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 50px;
-`;
-export const DiaryCountTitle = styled.div`
-  padding-top: 10px;
-  font-size: 15px;
-`;
-export const DiaryCountNumber = styled.div`
-  font-size: 15px;
-  font-weight: 700;
-  margin-left: 10px;
-  padding-top: 10px;
+export const ContentCard = styled.img`
+  width: 300px;
+  height: 300px;
+  background-color: black;
 `;
