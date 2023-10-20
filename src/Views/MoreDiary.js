38ad4bc8c4910b7d43db33a415bf49a8860e5a9d
audwo <kmjstj3@naver.com>
2023-10-21 02:36:33 +0900

    MoreDiary Page

--- src/Views/MoreDiary.js
+++ src/Views/MoreDiary.js
@@ -1,64 +1,115 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Section1 from "../Components/Section1/Section1";
-import HomeWraper from "../StyledComponents/HomeStyle/HomeWrapper";
 import Section3 from "../Components/Section3/Section3";
-import Profile from "../Components/Profile";
 import {
   DiaryContentsWrapper,
   DiaryContent,
   DiaryDate,
   DiaryImg,
-  DiaryTitle,
   DiaryWrapper,
   DiaryNav,
 } from "../StyledComponents/DiaryStyle/Diary";
 
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 const MoreDiary = () => {
-  const textareaRef = useRef(null);
+  const { diaryId } = useParams();
+
+  const [diary, setDiary] = useState(null);
+
   useEffect(() => {
-    resize();
+    const fetchDiaryData = async () => {
+      try {
+        const response = await axios.get(
+          `/page/render-only-diaryinfo/${diaryId}`
+        );
+        setDiary({ ...response.data[0] });
+      } catch (error) {}
+    };
+
+    fetchDiaryData();
   }, []);
-
-  function resize() {
-    let textarea = textareaRef.current;
-
-    textarea.style.height = "0px";
-
-    let scrollHeight = textarea.scrollHeight;
-    let style = window.getComputedStyle(textarea);
-    let borderTop = parseInt(style.borderTop);
-    let borderBottom = parseInt(style.borderBottom);
-
-    textarea.style.height = scrollHeight + borderTop + borderBottom + "px";
+  if (diary) {
+    console.log("1111199999");
+    console.log(diary);
+    console.log("1111199999");
   }
 
-  window.addEventListener("resize", resize);
-
-  return (
-    <HomeWraper>
-      <Section1 />
-      <DiaryWrapper padding="60px 20px" width="50%" height="1500px">
-        <DiaryNav height="60px" padding="15px 20px 5px 20px">
-          <Profile />
-          <DiaryDate fontSize="30px">2023.09.19</DiaryDate>
-        </DiaryNav>
-        <DiaryContentsWrapper height="2000px" padding="40px 20px">
-          <DiaryTitle fontSize="40px" height="80px" />
-          <DiaryImg></DiaryImg>
-          <DiaryContent
-            id="target-textarea"
-            ref={textareaRef}
-            onChange={resize}
-            fontSize="20px"
-            width="100%"
-            padding="10px"
-          />
-        </DiaryContentsWrapper>
-      </DiaryWrapper>
-      <Section3 />
-    </HomeWraper>
-  );
+  if (diary) {
+    return (
+      <Wrapper>
+        <HomeWrapper2>
+          <Section1 />
+          <DiaryWrapper width="50%">
+            <MoreDiaryTitle>{diary.title}</MoreDiaryTitle>
+            <DiaryNav>
+              <MoreProfileWrapper>
+                <MoreProfileImg />
+                <MoreProfileName>{diary.User.nickname}</MoreProfileName>
+              </MoreProfileWrapper>
+              <DiaryDate>2023.09.19</DiaryDate>
+            </DiaryNav>
+            <DiaryContentsWrapper padding="40px 20px">
+              <DiaryImgWrapper>
+                <DiaryImg src={diary.img} />
+              </DiaryImgWrapper>
+              <DiaryContent>{diary.content}</DiaryContent>
+            </DiaryContentsWrapper>
+          </DiaryWrapper>
+          <Section3 />
+        </HomeWrapper2>
+      </Wrapper>
+    );
+  }
 };
 
 export default MoreDiary;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const HomeWrapper2 = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+`;
+
+export const MoreDiaryTitle = styled.span`
+  font-size: 30px;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+export const MoreProfileWrapper = styled.div`
+  display: flex;
+`;
+
+export const MoreProfileImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: black;
+`;
+
+export const MoreProfileName = styled.div`
+  font-size: 15px;
+  margin-left: 10px;
+`;
+
+export const DiaryImgWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  margin-bottom: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
