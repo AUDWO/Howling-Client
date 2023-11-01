ceec2a284c158de1f3ea1ae7891ff522b074dd8b
audwo <kmjstj3@naver.com>
2023-11-01 22:52:14 +0900

    ProfileContentsConfigModal Component

---
+++ src/Components/Profile/ProfileContentConfig/ProfileContentsConfigModalCp.js
@@ -0,0 +1,197 @@
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import ModalOpenAtom from "../../../store/ModalOpenAtom";
+
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+import PostContentConfigOption from "./PostContentConfigOption";
+import DiaryContentConfigOption from "./DiaryContentConfigOption";
+import contentInfoAtom from "../../../store/contentInfo/diaryContentInfoAtom";
+import axios from "axios";
+
+const ProfileContentConfigModalCp = () => {
+  const profileContentConfigModalBackground = useRef();
+  const setContentConfigModalOpen = useSetRecoilState(
+    ModalOpenAtom("profileContentConfigModal")
+  );
+
+  const [diaryInfoOpen, setDiaryInfoOpen] = useState(false);
+  const [postInfoOpen, setPostInfoOpen] = useState(false);
+
+  const contentInfo = useRecoilValue(contentInfoAtom);
+
+  useEffect(() => {
+    if (contentInfo.type === "diary") {
+      setDiaryInfoOpen(true);
+    }
+
+    if (contentInfo.type === "post") {
+      setPostInfoOpen(true);
+    }
+  }, []);
+
+  const handleDelete = async () => {
+    if (diaryInfoOpen) {
+      await axios.delete(`/delete/diary/${contentInfo.id}`);
+    }
+
+    if (postInfoOpen) {
+      await axios.delete(`/delete/post/${contentInfo.id}`);
+    }
+  };
+
+  if (diaryInfoOpen || postInfoOpen) {
+    return (
+      <ProfileConfigModalWrapper
+        ref={profileContentConfigModalBackground}
+        onClick={(e) => {
+          if (e.target === profileContentConfigModalBackground.current) {
+            setContentConfigModalOpen(false);
+          }
+        }}
+      >
+        <ProfileConfigWrapper>
+          <ProfileConfigListItem>
+            <ConfigTitleWrapper>
+              <ConfigTitle>
+                {diaryInfoOpen && "일기 정보 변경"}
+                {postInfoOpen && "게시물 정보 변경"}
+              </ConfigTitle>
+            </ConfigTitleWrapper>
+            {diaryInfoOpen && (
+              <DiaryContentConfigOption contentInfo={contentInfo} />
+            )}
+            {postInfoOpen && (
+              <PostContentConfigOption contentInfo={contentInfo} />
+            )}
+          </ProfileConfigListItem>
+          <ProfileConfigListItem>
+            <ConfigTitleWrapper
+              onClick={() => {
+                handleDelete();
+              }}
+            >
+              <ConfigTitle color={"red"}>
+                {diaryInfoOpen && "일기 삭제"}
+                {postInfoOpen && "게시물 삭제"}
+              </ConfigTitle>
+            </ConfigTitleWrapper>
+          </ProfileConfigListItem>
+          <ProfileConfigListItem>
+            <ConfigTitleWrapper
+              onClick={() => {
+                setContentConfigModalOpen(false);
+              }}
+            >
+              <ConfigTitle>닫기</ConfigTitle>
+            </ConfigTitleWrapper>
+          </ProfileConfigListItem>
+        </ProfileConfigWrapper>
+      </ProfileConfigModalWrapper>
+    );
+  }
+};
+
+export default ProfileContentConfigModalCp;
+
+let marginDirection = {
+  marginL: "margin-left",
+  marginR: "margin-right",
+  marginB: "margin-bottom",
+  marginT: "margin-top",
+};
+
+let paddingDirection = {
+  paddingT: "padding-top",
+  paddingL: "padding-left",
+  paddingB: "padding-bottom",
+  paddingR: "padding-right",
+};
+
+let paddingDynamic = (props) => {
+  let paddingValue = ``;
+
+  for (const key in props) {
+    if (paddingDirection[key]) {
+      paddingValue += `
+          ${paddingDirection[key]} : ${props[key]}px
+          `;
+    }
+  }
+
+  return paddingValue;
+};
+
+let marginDynamic = (props) => {
+  let marginValue = ``;
+
+  for (const key in props) {
+    if (marginDirection[key]) {
+      marginValue += `
+          ${marginDirection[key]} : ${props[key]}px
+          `;
+    }
+  }
+
+  return marginValue;
+};
+
+export const ProfileConfigModalWrapper = styled.div`
+    position:fixed;
+    z-index: 17;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  `;
+
+export const ProfileConfigWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  z-index: 20;
+  width: 500px;
+  height: auto;
+  background-color: white;
+  border-radius: 10px;
+  opacity: 1;
+`;
+
+export const ProfileConfigListItem = styled.div`
+  width: 100%;
+  height: auto;
+  border: 0.5px solid gray;
+  color: gray;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const ConfigTitle = styled.div`
+  display: flex;
+  align-items: center;
+
+  color: ${(props) => (props.color ? props.color : "")};
+  height: 40px;
+`;
+
+export const ConfigTitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const OpenIcon = styled(MdOutlineKeyboardArrowDown)`
+  font-size: 30px;
+  cursor: pointer;
+`;
+
+export const CloseIcon = styled(MdOutlineKeyboardArrowUp)`
+  font-size: 30px;
+  cursor: pointer;
+`;
